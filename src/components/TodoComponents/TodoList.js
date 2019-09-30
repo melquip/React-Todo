239692071336e3425df341c06ecb08543103cba7
Melquisedeque Pereira <melqui1@live.com.pt>
2019-09-30 13:21:29 +0100

    Added todo's data and mapped over it

--- src/components/TodoComponents/TodoList.js
+++ src/components/TodoComponents/TodoList.js
@@ -1,2 +1,19 @@
+import React from 'react';
+import Todo from './Todo';
 // your components will all go in this `component` directory.
 // feel free to change this component.js into TodoList.js
+export default class TodoList extends React.Component {
+	render() {
+		return (
+			<div className="todolist">
+				{
+					this.props.data.map(todo =>
+						<Todo
+							data={todo}
+							toggleComplete={this.props.toggleComplete}
+						/>)
+				}
+			</div>
+		);
+	}
+}
